d843d20c6f7535c8e04282394034fd5e410df187
Tejasd12345 <85694469+Tejasd12345@users.noreply.github.com>
2022-06-07 20:00:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+img = "";
+status = "";
+objects = [] ;
+
+function setup() {
+ canvas = createCanvas(640,420);
+ canvas.center();
+ 
+objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML = "status : detecting objects";
+}
+function preload() {
+img = loadImage('school.jpg');    
+}
+
+function modelLoaded() {
+console.log("model is loaded by Tejas.D");
+status = true; 
+objectDetector.detect(img, gotResults); 
+}
+
+function draw() {
+image(img,0,0,640,420);
+if (status != "") {
+for (i = 0;i < objects.length;i++) {
+document.getElementById("status").innerHTML = " Status : object detected ";
+
+fill("#FF0000");
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + " " + percent + "%" , objects[i].x +10 , objects[i].y +13);
+noFill();
+stroke("#00FF00");
+rect(objects[i].x, objects[i].y, objects[i].width , objects[i].height);
+}    
+}
+
+}
+
+function gotResults(error,results) {
+if (error) {
+console.log(error);  
+}
+console.log(results); 
+objects = results;  
+}
+
